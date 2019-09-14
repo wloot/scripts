baed08d097b88a09e5dc97d2e916df634d2f03d0
Julian Liu <wlootlxt123@gmail.com>
2019-09-14 18:17:28 +0800

    clang

--- buildkernel.sh
+++ buildkernel.sh
@@ -7,21 +7,23 @@ export KJOBS="$((`grep -c '^processor' /proc/cpuinfo` * 2))"
 
 function clone_clang()
 {
-  CLANG_VERSION="clang-r353983e"
-  git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 clang
-  cd clang
-  find . | grep -v ${CLANG_VERSION} | xargs rm -rf
-  CLANG_PATH="${PWD}/${CLANG_VERSION}"
+  CLANG_VERSION="google clang 9.0.6"
+#  git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 clang
+#  cd clang
+#  find . | grep -v ${CLANG_VERSION} | xargs rm -rf
+#  CLANG_PATH="${PWD}/${CLANG_VERSION}"
+  git clone https://github.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-5799447 clang --depth=1
+  CLANG_PATH="${PWD}/clang"
   cd ..
 }
 
 function clone_proton_clang()
 {
-  curl https://kdrag0n.dev/files/toolchains/proton_clang-10.0.0-20190818-full.tar.zst | tar -I zstd -xf -
+  curl https://kdrag0n.dev/files/toolchains/proton_clang-10.0.0-20190915-full.tar.zst | tar -I zstd -xf -
   CLANG_VERSION="CLANG 10"
   mv proton_clang* clang
-  echo "deb http://archive.ubuntu.com/ubuntu eoan main" >> /etc/apt/sources.list && apt-get update
-  apt-get install libc6 libstdc++6 libgnutls30 -y
+#  echo "deb http://archive.ubuntu.com/ubuntu eoan main" >> /etc/apt/sources.list && apt-get update
+#  apt-get install libc6 libstdc++6 libgnutls30 -y
   CLANG_PATH="${PWD}/clang"
   GCC64="${CLANG_PATH}/bin/aarch64-linux-gnu-"
   GCC32="${CLANG_PATH}/bin/arm-linux-gnueabi-"
@@ -33,8 +35,8 @@ function clone_gcc()
   GCC64_TYPE="aarch64-elf-"
   GCC32_TYPE="arm-eabi-"
   GCC_VERSION="GCC 9"
-  git clone https://github.com/kdrag0n/${GCC64_TYPE}gcc
-  git clone https://github.com/kdrag0n/${GCC32_TYPE}gcc
+  git clone https://github.com/kdrag0n/${GCC64_TYPE}gcc --depth=1
+  git clone https://github.com/kdrag0n/${GCC32_TYPE}gcc --depth=1
   GCC64="${ccache} ${PWD}/${GCC64_TYPE}gcc/bin/${GCC64_TYPE}"
   GCC32="${ccache} ${PWD}/${GCC32_TYPE}gcc/bin/${GCC32_TYPE}"
 }
@@ -61,7 +63,7 @@ function build_clang()
 {
   rm -rf ${1}/arch/arm64/boot
   make O=${1} ARCH=arm64 ${2}_defconfig
-  make -j${KJOBS} O=${1} ARCH=arm64 CC="${CLANG}" CLANG_TRIPLE=${GCC64_TYPE} CROSS_COMPILE="${GCC64}" CROSS_COMPILE_ARM32="${GCC32}"
+  make -j${KJOBS} O=${1} ARCH=arm64 CC="clang" AR="llvm-ar" NM="llvm-nm" OBJCOPY="llvm-objcopy" OBJDUMP="llvm-objdump" STRIP="llvm-strip" CROSS_COMPILE="${GCC64}" CROSS_COMPILE_ARM32="${GCC32}" CLANG_TRIPLE="${GCC64_TYPE}"
   if [ $? -ne 0 ]; then
     errored "为${2}构建时出错， 终止。。。"
   fi
@@ -115,8 +117,8 @@ cd ${HOME}
 #install_ubuntu_gcc
 if [[ "$@" =~ "clang" ]]; then
   clone_proton_clang
-  CLANG="${ccache} ${CLANG_PATH}/bin/clang"
-  export PATH=${CLANG_PATH}/bin:$PATH LD_LIBRARY_PATH=${CLANG_PATH}/lib:$LD_LIBRARY_PATH
+  export LD_LIBRARY_PATH="${CLANG_PATH}/lib:${CLANG_PATH}/lib64:$LD_LIBRARY_PATH"
+  export PATH="${CLANG_PATH}/bin:$PATH"
 fi
 
 OUT_DIR=${HOME}/out
